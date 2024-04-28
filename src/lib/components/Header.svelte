013c4df47a6b7d69e33181a286d1c492aacb775b
gursheyss <rkwarya@gmail.com>
2024-04-27 17:06:11 -0700

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import LightSwitch from './LightSwitch.svelte';
+	import Button from './ui/button/button.svelte';
+</script>
+
+<div class="flex items-center justify-between p-2">
+	<h1 class="text-2xl font-bold">lendingsite</h1>
+	<div class="flex items-center space-x-2">
+		<Button>Sign in</Button>
+		<LightSwitch></LightSwitch>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
+<Header></Header>
+<ModeWatcher></ModeWatcher>
 <slot></slot>
